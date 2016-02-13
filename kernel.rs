74170d675e9b14fdabfee0571e60ca91f383c61a
Omar Rizwan <omar.rizwan@gmail.com>
2016-02-12 22:19:42 -0800

    Blink activity LED.
    (Still haven't tested.)

--- kernel.rs
+++ kernel.rs
@@ -1,14 +1,29 @@
 // kernel.rs
-#![feature(lang_items)]
+#![feature(lang_items, asm)]
 #![crate_type = "staticlib"]
 #![no_std]
 
-pub extern fn main() {
-    loop {}
+const GPIO_BASE: u32 = 0x20200000;
+
+fn sleep(value: u32) {
+    for _ in 1..value {
+        unsafe { asm!(""); }
+    }
 }
 
-#[lang = "eh_personality"]
-extern fn eh_personality() {}
+#[no_mangle]
+pub extern fn main() {
+    let gpio = GPIO_BASE as *const u32;
+    let led_on = unsafe { gpio.offset(8) as *mut u32 };
+    let led_off = unsafe { gpio.offset(11) as *mut u32 };
 
-#[lang = "panic_fmt"]
-extern fn panic_fmt() {}
+    loop {
+        unsafe { *(led_on) = 1 << 15; }
+        sleep(500000);
+        unsafe { *(led_off) = 1 << 15; }
+        sleep(500000);
+    }
+}
+
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] extern fn panic_fmt() {}
